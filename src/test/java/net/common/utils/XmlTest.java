add49cc193c374d2c1ee152a742535650c32cbe4
krisjin <krisibm@163.com>
2015-06-30 22:29:40 +0800

    update xml

--- src/test/java/net/common/utils/XmlTest.java
+++ src/test/java/net/common/utils/XmlTest.java
@@ -1,9 +1,13 @@
 package net.common.utils;
 
+import net.common.utils.config.xml.client.Group;
+import net.common.utils.config.xml.client.ServerElement;
+import net.common.utils.config.xml.client.ServiceGroup;
 import net.common.utils.config.xml.server.Server;
 import net.common.utils.config.xml.server.Servers;
 import net.common.utils.xml.JAXBUtil;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -22,10 +26,30 @@ public class XmlTest {
         Map<String, Server> map = servers.getServers();
         Set<Map.Entry<String, Server>> entrySet = map.entrySet();
         for (Map.Entry<String, Server> entry : entrySet) {
-
             String name = entry.getKey();
             Server server = entry.getValue();
-            System.out.println(server);
+            System.out.println(name + server);
         }
+
+
+        ServiceGroup serviceGroup = JAXBUtil.unmarshal(ServiceGroup.class, "redis-client.xml");
+
+        Map<String, Group> groupMap = serviceGroup.getGroups();
+
+        Set<Map.Entry<String, Group>> groupSet = groupMap.entrySet();
+
+        for (Map.Entry<String, Group> entry : groupSet) {
+
+            System.out.println(entry.getKey());
+            Group group = entry.getValue();
+            Group.ServerList serverList = group.getServerList();
+
+            List<ServerElement> serverElements = serverList.getServerElements();
+
+            for (ServerElement se : serverElements) {
+                System.out.println(se.getName());
+            }
+        }
+
     }
 }
