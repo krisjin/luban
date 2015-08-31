e3806bd1dc87958ca61fd46492667faff347126d
krisjin <krisibm@163.com>
2015-08-31 22:44:14 +0800

    add uuid util

---
+++ src/main/java/net/common/utils/uuid/UuidUtil.java
@@ -0,0 +1,38 @@
+package net.common.utils.uuid;
+
+import java.util.UUID;
+
+public final class UuidUtil {
+
+    private UuidUtil() {
+    }
+
+    /**
+     * 原始UUID的连接符
+     */
+    private static final String STR_LINK = "-";
+
+    /**
+     * 被替换成空字符串
+     */
+    private static final String STR_NULL = "";
+
+    /**
+     * 生成原始带分隔符的UUID
+     *
+     * @return
+     */
+    public static String genOriginUuid() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 生成去除分隔符的UUID
+     *
+     * @return
+     */
+    public static String genTerseUuid() {
+        return genOriginUuid().replace(STR_LINK, STR_NULL);
+    }
+
+}
