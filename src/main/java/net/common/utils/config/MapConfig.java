daa63b9b340a9e6295628563d1a203310631a322
krisjin <krisibm@163.com>
2015-08-31 11:40:37 +0800

    udpate config

--- src/main/java/net/common/utils/config/MapConfig.java
+++ src/main/java/net/common/utils/config/MapConfig.java
@@ -25,85 +25,9 @@ public class MapConfig {
 
     private String confPath;
 
-    /**
-     * 从配置中取得int类型的值,如果name不存在,则返回默认值
-     *
-     * @param name
-     * @param config
-     * @param defaultValue
-     * @return
-     */
-    public static int getInt(@Nonnull String name, @Nullable Map<String, String> config, int defaultValue) {
-        Preconditions.checkArgument(name != null, "name");
-        if (config == null) {
-            return defaultValue;
-        }
-        String s = config.get(name);
-        if (s == null) {
-            return defaultValue;
-        }
-        return Integer.parseInt(s);
+    private MapConfig() {
     }
 
-    /**
-     * 从配置中取得byte类型的值,如果name不存在,则返回默认值
-     *
-     * @param name
-     * @param config
-     * @param defaultValue
-     * @return
-     */
-    public static byte getByte(@Nonnull String name, @Nullable Map<String, String> config, byte defaultValue) {
-        Preconditions.checkArgument(name != null, "name");
-        if (config == null) {
-            return defaultValue;
-        }
-        String s = config.get(name);
-        if (s == null) {
-            return defaultValue;
-        }
-        return Byte.parseByte(s);
-    }
-
-    /**
-     * 从配置中取得long类型的值,如果name不存在,则返回默认值
-     *
-     * @param name
-     * @param config
-     * @param defaultValue
-     * @return
-     */
-    public static long getLong(@Nonnull String name, @Nullable Map<String, String> config, long defaultValue) {
-        Preconditions.checkArgument(name != null, "name");
-        if (config == null) {
-            return defaultValue;
-        }
-        String s = config.get(name);
-        if (s == null) {
-            return defaultValue;
-        }
-        return Long.parseLong(s);
-    }
-
-    /**
-     * 从配置中取得String类型的值,如果name不存在,则返回默认值
-     *
-     * @param name
-     * @param config
-     * @param defaultValue
-     * @return
-     */
-    public static String getString(@Nonnull String name, @Nullable Map<String, String> config, String defaultValue) {
-        Preconditions.checkArgument(name != null, "name");
-        if (config == null) {
-            return defaultValue;
-        }
-        String s = config.get(name);
-        if (s == null) {
-            return defaultValue;
-        }
-        return s;
-    }
 
     /**
      * 从配置文件中取得boolean类型的值,如果name不存在,则返回默认值<code>defaultValue</code>
