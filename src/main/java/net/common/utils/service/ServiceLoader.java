c5c12885013de7cabf8dea3f3e1e47838679e482
krisjin <krisibm@163.com>
2015-09-10 11:00:22 +0800

    update readme.md and update class notes

--- src/main/java/net/common/utils/service/ServiceLoader.java
+++ src/main/java/net/common/utils/service/ServiceLoader.java
@@ -3,7 +3,10 @@ package net.common.utils.service;
 import com.google.common.cache.CacheLoader;
 
 /**
- * 介绍：一个通用的抽象类，开发的同学如果希望开发一个新的服务时候只需要继承这个抽象类，实现里面的load方法即可。（当然还需要实现对应的一套服务类，每个服务类提供了所有支持的操作）
+ * 一个通用的抽象类,要开发一个新的服务时候只需要继承这个抽象类，实现里面的load方法即可。
+ * （当然还需要实现对应的一套服务类，每个服务类提供了所有支持的操作）
+ *
+ * @param <T>
  */
 public abstract class ServiceLoader<T> extends CacheLoader<String, T> {
 
