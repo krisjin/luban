468bd2c12d6104259580ba79108447bd702192a1
krisjin <krisibm@163.com>
2015-09-30 18:20:04 +0800

    邮件工具类更新

--- src/test/java/net/common/utils/MailTest.java
+++ src/test/java/net/common/utils/MailTest.java
@@ -4,18 +4,20 @@ import net.common.utils.mail.MailInfo;
 import net.common.utils.mail.MailUtil;
 
 public class MailTest {
-	
-	public static void main(String[] args) throws Exception{
-		MailInfo info =new MailInfo();
-		
-		info.setHtmlBody("<div style='font-size:18px;'>Hellowrod</div>");
-		info.setMailFrom("krisibm@163.com");
-		info.setMailTo("shijingui@staff.hexun.com");
-		info.setMailSubject("2015");
-		info.setUsername("krisibm@163.com");
-		info.setPassword("");
-		info.setMailServerHost("smtp.163.com");
-		MailUtil.sendAttachEmail(info, new String[]{"d:/daocache.rar"});
-	}
+
+    public static void main(String[] args) throws Exception {
+        MailInfo info = new MailInfo();
+
+        info.setHtmlBody("<div style='font-size:18px;'>Hellowrod</div>");
+        info.setMailFrom("krisibm@163.com");
+        info.setMailTo("jinguishi@tianler.com");
+        info.setMailSubject("带人的艺术与-Kindle版");
+        info.setUsername("krisibm@163.com");
+        info.setPassword("guidao!@#");
+        info.setMailServerHost("smtp.163.com");
+        info.setIsAuth(true);
+        info.setMailName("NIHO,中午好!");
+        MailUtil.sendAttachEmail(info, new String[]{"e:/带人的艺术与.mobi"});
+    }
 
 }
