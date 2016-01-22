83409deb62c37c017460c7302f47e7b14c9a261e
shijingui <shijingui@jd.com>
2016-01-22 13:04:40 +0800

    update httpclient

--- src/main/java/net/common/utils/http/HttpClientUtil.java
+++ src/main/java/net/common/utils/http/HttpClientUtil.java
@@ -1,16 +1,12 @@
 package net.common.utils.http;
 
 import com.google.common.net.HttpHeaders;
-import org.apache.http.Header;
-import org.apache.http.HeaderElement;
 import org.apache.http.HttpStatus;
-import org.apache.http.ParseException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,12 +29,11 @@ public final class HttpClientUtil {
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         HttpGet httpGet = new HttpGet(url);
-        Header header =new BasicHeader();
         httpGet.addHeader(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;");
         httpGet.addHeader(HttpHeaders.ACCEPT_LANGUAGE, "zh-cn");
         httpGet.addHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3");
-        httpGet.addHeader(HttpHeaders.CACHE_CONTROL,"no-cache");
-        httpGet.addHeader(HttpHeaders.CONNECTION,"Keep-Alive");
+        httpGet.addHeader(HttpHeaders.CACHE_CONTROL, "no-cache");
+        httpGet.addHeader(HttpHeaders.CONNECTION, "Keep-Alive");
         RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(2 * 1000).setConnectionRequestTimeout(1 * 1000).setSocketTimeout(2 * 1000).build();
 
         httpGet.setConfig(requestConfig);
