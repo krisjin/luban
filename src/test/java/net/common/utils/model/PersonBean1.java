31ef5e0689d07dcd14ac2a46a868869350907c21
shijingui <shijingui@jd.com>
2016-01-22 18:24:34 +0800

    add bean copy

---
+++ src/test/java/net/common/utils/model/PersonBean1.java
@@ -0,0 +1,58 @@
+package net.common.utils.model;
+
+/**
+ * User: shijingui
+ * Date: 2016/1/22
+ */
+public class PersonBean1 {
+
+    private String username;
+
+    private String account;
+
+    private int sex;
+
+    private String favorit;
+
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getFavorit() {
+        return favorit;
+    }
+
+    public void setFavorit(String favorit) {
+        this.favorit = favorit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/test/java/net/common/utils/model/PersonBean2.java
@@ -0,0 +1,37 @@
+package net.common.utils.model;
+
+/**
+ * User: shijingui
+ * Date: 2016/1/22
+ */
+public class PersonBean2 {
+    private String username;
+
+    private String favorit;
+
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFavorit() {
+        return favorit;
+    }
+
+    public void setFavorit(String favorit) {
+        this.favorit = favorit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
