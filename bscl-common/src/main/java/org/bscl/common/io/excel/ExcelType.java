fcd5af83546e7be8071c81708c6f0a9df10f42c0
shijingui <shijingui@jd.com>
2019-01-20 08:58:36 +0800

    update package

--- bscl-common/src/main/java/org/bscl/common/io/excel/ExcelType.java
+++
@@ -1,9 +0,0 @@
-package org.bscl.common.io.excel;
-
-/**
- * User: shijingui
- * Date: 2016/1/29
- */
-public enum ExcelType {
-    XLSX,XLS
-}
--- bscl-common/src/test/java/org/bscl/common/enums/ProcessStatus.java
+++
@@ -1,9 +0,0 @@
-package org.bscl.common.enums;
-
-/**
- * User: shijingui
- * Date: 2016/10/9
- */
-public enum ProcessStatus {
-    NO, YES;
-}
