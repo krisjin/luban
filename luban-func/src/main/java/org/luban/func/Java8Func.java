21e2d983c3ed842d0d05514e6037571dbef77b3c
krisjin <krisibm@163.com>
2023-07-15 17:09:17 +0800

    2

--- luban-func/src/main/java/org/luban/func/Java8Func.java
+++ luban-func/src/main/java/org/luban/func/Java8Func.java
@@ -45,19 +45,49 @@ public class Java8Func {
     }
 
     private static void test6(List<Person> persons) {
-        Integer ageSum = persons
-                .parallelStream()
-                .reduce(0,
-                        (sum, p) -> {
-                            System.out.format("accumulator: sum=%s; person=%s; thread=%s\n",
-                                    sum, p, Thread.currentThread().getName());
-                            return sum += p.age;
-                        },
-                        (sum1, sum2) -> {
-                            System.out.format("combiner: sum1=%s; sum2=%s; thread=%s\n",
-                                    sum1, sum2, Thread.currentThread().getName());
-                            return sum1 + sum2;
-                        });
+        Integer ageSum = persons.parallelStream().reduce(0, (sum, p) -> {
+            System.out.format("accumulator: sum=%s; person=%s; thread=%s\n", sum, p, Thread.currentThread().getName());
+            return sum += p.age;
+        }, (sum1, sum2) -> {
+            System.out.format("combiner: sum1=%s; sum2=%s; thread=%s\n", sum1, sum2, Thread.currentThread().getName());
+            return sum1 + sum2;
+        });
+
+        System.out.println(ageSum);
+    }
+
+    private static void test3(List<String> strings) {
+        strings.parallelStream().filter(s -> {
+            System.out.format("filter:  %s [%s]\n", s, Thread.currentThread().getName());
+            return true;
+        }).map(s -> {
+            System.out.format("map:     %s [%s]\n", s, Thread.currentThread().getName());
+            return s.toUpperCase();
+        }).sorted((s1, s2) -> {
+            System.out.format("sort:    %s <> %s [%s]\n", s1, s2, Thread.currentThread().getName());
+            return s1.compareTo(s2);
+        }).forEach(s -> System.out.format("forEach: %s [%s]\n", s, Thread.currentThread().getName()));
+    }
+
+    private static void test2(List<Person> persons) {
+        Person result = persons.stream().reduce(new Person("", 0), (p1, p2) -> {
+            p1.age += p2.age;
+            p1.name += p2.name;
+            return p1;
+        });
+
+        System.out.format("name=%s; age=%s", result.name, result.age);
+    }
+
+
+    private static void test4(List<Person> persons) {
+        Integer ageSum = persons.stream().reduce(0, (sum, p) -> {
+            System.out.format("accumulator: sum=%s; person=%s\n", sum, p);
+            return sum += p.age;
+        }, (sum1, sum2) -> {
+            System.out.format("combiner: sum1=%s; sum2=%s\n", sum1, sum2);
+            return sum1 + sum2;
+        });
 
         System.out.println(ageSum);
     }
